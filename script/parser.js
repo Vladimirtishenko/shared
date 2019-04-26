c3a514fe5147384f0c86205001956afe1d4e9161
Vladimir Tishenko <Vladimirtishenko1@gmail.com>
2019-04-26 11:44:18 +0300

    Update parser.js

--- script/parser.js
+++ script/parser.js
@@ -7,15 +7,15 @@ module.exports = function (url) {
 
               if (!err) {
 
-                  let $ = Cheerio.load(body),
-                      title = $('title').text() || $('meta[name="twitter:title"]').attr('content') || $('meta[property="og:title"]').attr('content') || $('h1').text().split("#")[0],
-                      description = $('meta[name="description"]').attr('content') || $('meta[name="twitter:description"]').attr('content') || $('meta[property="og:description"]').attr('content') || $('h2').text().split("#")[0],
-                      img = $('meta[property="og:image"]').attr('content') || $('meta[name="twitter:image"]').attr('content');                
+                  const $ = Cheerio.load(body),
+                        title = $('title').text() || $('meta[name="twitter:title"]').attr('content') || $('meta[property="og:title"]').attr('content') || $('h1').text().split("#")[0],
+                        description = $('meta[name="description"]').attr('content') || $('meta[name="twitter:description"]').attr('content') || $('meta[property="og:description"]').attr('content') || $('h2').text().split("#")[0],
+                        img = $('meta[property="og:image"]').attr('content') || $('meta[name="twitter:image"]').attr('content');                
 
-                  return {title, description, img, url};
+                  return {title, description, img};
 
               } else {
-                  res.status(500).send({ err });
+                   throw new Error(err);
               }
       });
 
