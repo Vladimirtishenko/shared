857e27ac30a7222e48e003f8a341d2cf49909313
Владимир Тищенко <valdimirtishenko>
2019-12-15 10:35:34 +0200

    Add validation eslint

--- validation.helper.js
+++ validation.helper.js
@@ -1,16 +1,16 @@
-export const required = value => value ? undefined : 'Required field';
+export const required = value => (value ? undefined : 'Required field');
 
-export const symbols = value => {
+export const symbols = (value) => {
     const test = /[A-Z\s\*\&\!\@\#\$\%\^\(\)\{\}\[\]\?\'\'\\\/\,\+\-\|\:\;\~\`]/.test(value);
-    return test && 'Wrong label format!' || undefined;
-}
+    return (test && 'Wrong label format!') || undefined;
+};
 
-export const numbers = value => {
+export const numbers = (value) => {
     const test = /^-?[0-9]\d*(\d+)?$/.test(value);
-    return !test && 'This field should contain numbers only' || undefined;
-}
+    return (!test && 'This field should contain numbers only') || undefined;
+};
 
-export const email = value => {
+export const email = (value) => {
     const test = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
-    return !test && 'This field should be a valid e-mail' || undefined;
-}
+    return (!test && 'This field should be a valid e-mail') || undefined;
+};
